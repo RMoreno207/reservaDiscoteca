f7a94ebb19d32ddfbe4a20f5efac984e295178b4
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-09-29 15:59:48 +0200

    refactorized

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NotFound extends Component {
-  render() {
-    return <div>NotFound</div>;
-  }
-}
+const NotFound = () => {
+  return <div>Not Found</div>;
+};
 
 export default NotFound;
