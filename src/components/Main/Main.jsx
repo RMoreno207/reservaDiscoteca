ee7007441e79b98ae7843233660cf1ef1f2d9146
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-09-29 15:48:42 +0200

    folders created

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import Singup from './Singup/Singup';
+import Booking from './Booking/Booking';
+import NotFound from '../NotFound/NotFound';
 
-class Main extends Component {
-  render() {
-    return <div>Main</div>;
-  }
-}
+const Main = () => {
+  return <div>
+
+    <Routes>
+      <Route element={<Home />} path='/' />
+      <Route element={<Singup />} path='/singup' />
+      <Route element={<Booking />} exact path='/booking' />
+      <Route element={<NotFound />} path={"/*"} />
+    </Routes>
+
+
+
+  </div>;
+};
 
 export default Main;
