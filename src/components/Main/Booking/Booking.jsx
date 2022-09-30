4ec54720243bc901161fefa5e3553f75abee0f73
RMoreno207 <107397146+RMoreno207@users.noreply.github.com>
2022-09-30 12:31:55 +0200

    create alerts

--- src/components/Main/Booking/Booking.jsx
+++ src/components/Main/Booking/Booking.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
 const Booking = () => {
+  const showResults = (e) => {
+    e.preventDefault();
+    console.log("event ", e);
+    console.log("event zona ", e.target.zone.value, e.target.date.value);
+    alert("Su reserva ha sido recibida\n"
+      + "Datos de la reserva:"
+      + "\nNombre: "
+      + e.target.firstName.value
+      + "\nFecha: "
+      + e.target.date.value
+      + "\nHora: "
+      + e.target.time.value
+      + "\nZona: "
+      + e.target.zone.value);
+  }
   return (
     <div className="Reserva">
+
       <div >
         <h1>Reserva</h1>
-        <form className="Form_reserva">
-          <label className="Ttulo" htmlFor="firstName">Nombre</label>
+        <form className="Form_reserva" onSubmit={showResults}>
+          {/* <label className="Ttulo" htmlFor="firstName">Nombre</label> */}
           {/* Nombre */}
           <input className="input-sin-desplegar"
             id="firstName"
@@ -14,28 +30,35 @@ const Booking = () => {
             placeholder="Insertar Nombre"
           />
           {/* fecha */}
-          <input className="input-sin-desplegar"
-            id="date"
-            type="date"
-            name="date"
-            placeholder="Escoger fecha"
-          />
+          <select className="input-sin-desplegar" name="date" id="date">
+            <option value="date">Escoger fecha</option>
+            <option value="Lunes">Lunes</option>
+            <option value="Martes">Martes</option>
+            <option value="Miercoles">Miercoles</option>
+            <option value="Jueves">Jueves</option>
+            <option value="Viernes">Viernes</option>
+            <option value="Sabado">Sabado</option>
+            <option value="Domingo">Domingo</option>
+          </select>
           {/* Hora */}
-          <input className="input-sin-desplegar"
-            id="time"
-            type="time"
-            name="time"
-            placeholder="Hora"
-          />
+          <select className="input-sin-desplegar" name="time" id="time">
+            <option value="time">Hora</option>
+            <option value="19h">19:00</option>
+            <option value="20h">20:00</option>
+            <option value="21h">21:00</option>
+            <option value="22h">22:00</option>
+            <option value="23h">23:00</option>
+            <option value="00h">00:00</option>
+          </select>
           {/* Zona */}
           <select className="input-sin-desplegar" name="zone" id="zone" placeholder="Zona">
             <option value="zona">Zona</option>
-            <option value="vip">VIP</option>
-            <option value="palco">Palco</option>
-            <option value="terraza">Terraza</option>
-            <option value="ZonaA">Zona A</option>
-            <option value="ZonaB">Zona B</option>
-            <option value="ZonaC">Zona C</option>
+            <option value="VIP">VIP</option>
+            <option value="Palco">Palco</option>
+            <option value="Terraza">Terraza</option>
+            <option value="Zona A">Zona A</option>
+            <option value="Zona B">Zona B</option>
+            <option value="Zona C">Zona C</option>
           </select>
           {/* Email
 <input className="input-sin-desplegar"
