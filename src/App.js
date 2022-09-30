a217e60565873ca1c99455caa9f344fcee8f7f84
pablomoga9 <moyagarcia99@gmail.com>
2022-09-30 13:05:58 +0200

    check

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import './App.css';
 import 'normalize.css';
-
+import Singup from './components/Main/Singup/Singup';
 import Header from './components/Header/Header';
-
+import Main from './components/Main/Main'
 import '../src/styles/styles.scss';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+     <BrowserRouter>
+        <Header/>
+        <Main/>
+     </BrowserRouter>
+     <Footer/>
     </div>
   );
 }
